03e18c2d4e4d62ba458abe3e5cb64b2df0c6567e
Oscar Marginean <hello@oscar.sh>
2021-03-07 10:35:31 +0100

    Added start & exit functionality

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void QuitGame()
+    {
+
+        Debug.Log("Quitting game");
+        Application.Quit();
+    }
+}
