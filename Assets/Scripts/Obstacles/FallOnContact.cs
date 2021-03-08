ab96e8fed97890c5517ac9ca95f43cf183257a1f
Bouzidi, Ines <ines.bouzidi@gatech.edu>
2021-03-09 00:23:58 +0100

    Added obstacles to challenge section

---
+++ Assets/Scripts/Obstacles/FallOnContact.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallOnContact : MonoBehaviour
+{
+
+	public float fallDelay = 0.5f;
+
+	void OnCollisionEnter(Collision collision)
+	{
+		foreach (ContactPoint contact in collision.contacts)
+		{ 
+			if (collision.gameObject.tag == "Player")
+			{
+				StartCoroutine(Fall(fallDelay));
+			}
+		}
+	}
+
+	IEnumerator Fall(float time)
+	{
+
+		yield return new WaitForSeconds(time);
+
+		foreach (Transform child in this.transform)
+		{
+			child.gameObject.SetActive(false);
+			
+		} 
+
+		yield return new WaitForSeconds(15);
+		foreach (Transform child in this.transform)
+		{
+			child.gameObject.SetActive(true);
+
+		}
+		//Destroy(gameObject);
+	}
+
+
+}
