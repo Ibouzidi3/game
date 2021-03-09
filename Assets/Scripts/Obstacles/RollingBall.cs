f7782b7fa357fb626e0778dfa66a37f5c5cf1ba1
Bouzidi, Ines <ines.bouzidi@gatech.edu>
2021-03-10 00:49:54 +0100

    Added obstacles to challenge section

---
+++ Assets/Scripts/Obstacles/RollingBall.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollingBall : MonoBehaviour
+{
+    
+    public GameObject[] objectPool;
+    public Vector3[] positions;
+    private int currentIndex = 0;
+
+
+    float elapsedTime = 0f; // Counts up to repeatTime
+    public float repeatTime = 5f; // Time taken to repeat in seconds
+
+    float roundTime = 60f; // Time of one round
+    float totalRoundTime = 60f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        positions = new Vector3[objectPool.Length];
+        int i = 0;
+        foreach (GameObject obj in objectPool)
+        {
+            positions[i] = objectPool[i].transform.position;
+            obj.SetActive(false); 
+            i += 1;
+        }
+
+        gameObject.active = false;
+
+    }
+
+    private void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        if (elapsedTime >= repeatTime)
+        { 
+            NewRandomObject(); 
+            elapsedTime -= repeatTime;
+        }
+
+        totalRoundTime -= Time.deltaTime;
+        if (totalRoundTime <= 0)
+        {
+
+            repeatTime -= 0.5f;
+            totalRoundTime = 10f;
+        }
+    }
+
+    public void NewRandomObject()
+    {
+        int newIndex = Random.Range(0, objectPool.Length);
+        // Deactivate old gameobject
+        objectPool[currentIndex].SetActive(false);
+        // Activate new gameobject
+        currentIndex = newIndex;
+
+        objectPool[currentIndex].SetActive(true);
+        objectPool[currentIndex].transform.position = positions[currentIndex];
+    }
+ 
+
+
+
+}
+
---
+++ Assets/Scripts/Obstacles/RollingBallActivator.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollingBallActivator : MonoBehaviour
+{
+
+    public GameObject ball;
+
+    void OnTriggerEnter(Collider c)
+    {
+        if (c.attachedRigidbody != null)
+        {
+            if (c.gameObject.tag == "Player")
+            {
+                ball.active = true;
+
+            }
+
+
+        }
+
+
+    }
+
+    void OnTriggerExit(Collider c)
+    {
+        if (c.attachedRigidbody != null)
+        {
+
+            if (c.attachedRigidbody != null)
+            {
+                if (c.gameObject.tag == "Player")
+                {
+                    ball.active = false;
+
+                }
+
+
+            }
+
+        }
+
+    }
+
+}
