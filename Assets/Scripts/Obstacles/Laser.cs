155eae5f5d75e10e15350aa1b44d3cbcac83f997
Bouzidi, Ines <ines.bouzidi@gatech.edu>
2021-03-11 22:18:39 +0100

    Updated Challenge section

---
+++ Assets/Scripts/Obstacles/Laser.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    private LineRenderer lr;
+
+    // Use this for initialization
+    void Start()
+    {
+        lr = GetComponent<LineRenderer>(); 
+
+        if (lr == null)
+            Debug.Log("LineRenderer could not be found");
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            if (hit.collider)
+            {
+                lr.SetPosition(1, hit.point);
+            }
+        } 
+    }
+}
