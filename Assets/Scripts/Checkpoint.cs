e6e87ee913d3d73443e0e307e85bc006e6880ea0
Bouzidi, Ines <ines.bouzidi@gatech.edu>
2021-03-24 13:37:15 +0100

    checkpoint bug fix

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -16,11 +16,11 @@ public class Checkpoint : MonoBehaviour
 		{
 			other.gameObject.GetComponent<CharacterControls>().checkPoint = checkpoint.parent.position;
 			EventManager.TriggerEvent<CheckpointEvent, Vector3>(checkpoint.parent.position);
-			Vector3 position = new Vector3(other.transform.position.x, other.transform.position.y+1, other.transform.position.z+5);
+			Vector3 position = new Vector3(other.transform.position.x, other.transform.position.y+5, other.transform.position.z+5);
 			GameObject text = Instantiate(checkpointText, position, Quaternion.identity);
 			particle = text.GetComponent<ParticleSystem>(); 
 			particle.Play();
-			//transform.gameObject.SetActive(false);
+			transform.gameObject.SetActive(false);
 		}
 	}
 }
