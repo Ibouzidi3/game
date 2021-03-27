63d0158e00cc79e373c5cbca790c720c3c0a3b75
Miko Linsangan <linsangan.miko@gmail.com>
2021-03-28 00:39:54 +0800

    added coin collection text and some miscelaneous changes.

--- Assets/Scripts/Collectible/PlayerInterface/CoinCollectorComponent.cs
+++ Assets/Scripts/Collectible/PlayerInterface/CoinCollectorComponent.cs
@@ -1,10 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CoinCollectorComponent : MonoBehaviour
 {
     private int numberOfCoins = 0;
+    public TMP_Text textMeshPro;
+    public void Start()
+    {
+        if(GameObject.Find("coin_text") != null)
+            textMeshPro = GameObject.Find("coin_text").GetComponent<TMP_Text>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other != null && other.gameObject.CompareTag("Coin"))
@@ -13,6 +21,9 @@ public class CoinCollectorComponent : MonoBehaviour
             EventManager.TriggerEvent<CoinCollectionEvent, Vector3>(other.transform.position);
             other.gameObject.SetActive(false);
             numberOfCoins++;
+            if (textMeshPro != null)
+                textMeshPro.text = numberOfCoins.ToString();
+
         }
 
     }
