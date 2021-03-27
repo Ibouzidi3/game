382687d927aac85d945e16aa42c5f43c49032bff
Bouzidi, Ines <ines.bouzidi@gatech.edu>
2021-03-27 20:57:15 +0100

    laser bug fix

--- Assets/Scripts/Obstacles/Laser.cs
+++ Assets/Scripts/Obstacles/Laser.cs
@@ -15,18 +15,22 @@ public class Laser : MonoBehaviour
             Debug.Log("LineRenderer could not be found");
     }
 
+
     // Update is called once per frame
     void Update()
-    { 
+    {
+        lr.SetPosition(0, transform.position);
         RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.forward, out hit))
-        {
-            if (hit.collider)
-            {
-                lr.SetPosition(1, hit.point);
-                EventManager.TriggerEvent<LaserHitEvent, Vector3>(hit.point);
-            }
-        } 
+         if (Physics.Raycast(transform.position, transform.forward, out hit))
+         {
+             if (hit.collider)
+             {
+                 lr.SetPosition(1, hit.point);
+                 EventManager.TriggerEvent<LaserHitEvent, Vector3>(hit.point);
+             }
+         }
+        else lr.SetPosition(1, transform.forward * 5000); 
+
 
     }
 }
