af6ade1e704b93e07c8c321574b69ebb4196d63f
Bouzidi, Ines <ines.bouzidi@gatech.edu>
2021-04-10 16:51:09 +0100

    Updated main scene

---
+++ Assets/Scripts/Obstacles/FireLine.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireLine : MonoBehaviour
+{
+
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player") 
+        {
+            GameObject fire = GameObject.FindGameObjectWithTag("Fire");
+            if (fire.activeSelf)
+            {
+                other.SendMessage("ShootPlayer", other);
+            }
+            
+        }
+
+
+    }
+}
