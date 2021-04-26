a631a47037015c5c97100c67ec308b0aa6481bfa
Bouzidi, Ines <ines.bouzidi@gatech.edu>
2021-04-26 02:08:31 +0100

    More changes to Main Scene

---
+++ Assets/Scripts/Obstacles/Fountain.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fountain : MonoBehaviour
+{
+    public float damping = 2f;
+    public float force = 10.0f;
+    private Rigidbody rb ;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+
+            rb = other.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                transform.GetChild(0).gameObject.SetActive(true);
+                Vector3 velocity = rb.velocity;
+                velocity += transform.up * force * Time.deltaTime;
+                velocity -= velocity * damping * Time.deltaTime;
+                rb.velocity = velocity;
+                StartCoroutine("Float");
+            }
+
+        }
+
+
+    }
+
+    IEnumerator Float()
+    {
+        Debug.Log("Floating");
+        rb.useGravity = false;
+        yield return new WaitForSeconds(2);
+        rb.useGravity = true;
+        transform.GetChild(0).gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Obstacles/FountainPoison.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FountainPoison : MonoBehaviour
+{
+  
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            transform.GetChild(0).gameObject.SetActive(true);
+            other.SendMessage("ShootPlayer", other);
+
+        }
+
+    } 
+}
