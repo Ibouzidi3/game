a7708ac4e7fa2cbb54397bf849b3130a7a62183d
Bouzidi, Ines <ines.bouzidi@gatech.edu>
2021-04-26 18:24:20 +0100

    More colliders and fixes to main scene

---
+++ Assets/Crash.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crash : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        { 
+            other.SendMessage("ShootPlayer", other);
+
+        }
+
+    }
+}
