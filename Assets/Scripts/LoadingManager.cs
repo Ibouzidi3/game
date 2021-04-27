b000c52a1becf7ee4ff9cf1dafe8b8c1b056e35a
Bouzidi, Ines <ines.bouzidi@gatech.edu>
2021-04-27 14:32:41 +0100

    Added Loading bar to EndOfRace

--- Assets/Scripts/LoadingManager.cs
+++ Assets/Scripts/LoadingManager.cs
@@ -7,24 +7,55 @@ public class LoadingManager : MonoBehaviour
 {
     public Slider loadingBar;
     public Text loadingText;
+    public GameObject loadingPanel;
+    public Canvas menuCanvas;
 
-    void Start()
+
+    private string scene;
+    private CanvasGroup canvasGroup;
+
+    void Awake()
     {
-        LoadScene("Menu");
+
+        scene = SceneManager.GetActiveScene().name; 
+        if (scene == "LoadingScene")
+        {
+            LoadScene("Menu");
+        }
+        else
+        {
+            canvasGroup = menuCanvas.GetComponent<CanvasGroup>();
+            canvasGroup.interactable = true;
+            canvasGroup.blocksRaycasts = true;
+            canvasGroup.alpha = 1f;
+        }
+        
     }
 
     public void LoadScene(string sceneName)
-    {
+    { 
+        if (scene == "EndOfRace")
+        { 
+            loadingPanel.SetActive(true); 
+            canvasGroup.interactable = false;
+            canvasGroup.blocksRaycasts = false;
+            canvasGroup.alpha = 0f;
+
+
+        }
+
         StartCoroutine(LoadSceneAsync(sceneName));
     }
 
     IEnumerator LoadSceneAsync(string sceneName)
     {
+
         AsyncOperation op = SceneManager.LoadSceneAsync(sceneName);
 
         while (!op.isDone)
         {
-            float progress = Mathf.Clamp01(op.progress / .9f); 
+            float progress = Mathf.Clamp01(op.progress / .9f);
+            progress = Mathf.Round(progress * 100f) / 100f;
             loadingBar.value = progress;
             loadingText.text = progress * 100f + "%";
             yield return null;
